32da1d9f16ed69d8d6a417ce47cc36d3fcc85c54
Dylan Steele <dylansteele57@gmail.com>
2019-03-17 16:57:46 -0400

    started usage counter

--- src/fsdb.js
+++ src/fsdb.js
@@ -8,7 +8,20 @@ const collectionName = `${config['user-settings'].username}-Snippets`;
 function Add(model) {
     db.collection(collectionName).doc(model.name).set(model);
 }
+function Get(model) {
+    return db.collection(collectionName).doc(model.name);
+}
+function Update(args, utdObj) {
+    let objRef = Get(args);
+    objRef.update(utdObj).then(() => {
+        console.log('Updated');
+    }).catch((err) => {
+        console.log(err);
+    });
+}
 
 module.exports = {
-    Add
+    Add,
+    Update,
+    Get
 }
